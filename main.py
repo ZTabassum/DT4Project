f15fa11ec8d948ab184bd20c307d3ec64a240591
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 17:18:34 -0500

    Tempoarary don't use

--- main.py
+++ main.py
@@ -42,7 +42,11 @@ class Question(ndb.Model):
     answer= ndb.StringProperty()
 
 question1=Question(act_question= "How many moons does earth have?", category="Science", level= 1, answer="1")
-question2=Question(act_question= "Who is the first president of the United States?", category="Social Studies", level= 1, answer="George Washington")
+question2=Question(act_question= "If the number of protons in an element is seven, how many electrons does it have", category="Science", level= 4, answer="seven")
+question3=Question(act_question= "How many planets are there?", category="Science", level= 2, answer="Eight")
+question4=Question(act_question= "How many moons does Saturn have?", category="Science", level= 3, answer="Sixty Two")
+question5=Question(act_question= "What is the main element in Earth's atmosphere", category="Science", level= 1, answer="1")
+question5=
 
 
 class MainHandler(webapp2.RequestHandler):
